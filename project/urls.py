4d791cceb6441f7ee686360fe6994993afa547a0
Feel Fine <danielfeelfine@gmail.com>
2014-02-06 16:30:30 -0300

    Update urls.py
    scaffold from REST Framework

--- project/urls.py
+++ project/urls.py
@@ -1,12 +1,23 @@
 from django.conf.urls import patterns, include, url
 
+from rest_framework import routers
+from models import UserViewSet, GroupViewSet
 from django.contrib import admin
 admin.autodiscover()
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^', include(router.urls)),
+
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
