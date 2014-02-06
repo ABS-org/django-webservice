526edde3d4b1161529438c0c8524a699cc07a27b
Feel Fine <danielfeelfine@gmail.com>
2014-02-06 16:54:10 -0300

    Update settings.py
    Remove STATICFILES_DIRS

--- project/settings.py
+++ project/settings.py
@@ -84,10 +84,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
 
-# Additional locations of static files
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # List of finder classes that know how to find static files in
 # various locations.
