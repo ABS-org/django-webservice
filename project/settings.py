23d267c0bcdb64dffb3ce1ba31012c4c4f2fd5ab
Feel Fine <danielfeelfine@gmail.com>
2014-02-11 16:34:45 -0300

    Update settings.py
    Add apps.uploader in INSTALLED_APPS

--- project/settings.py
+++ project/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    'project.apps.uploader',
     'rest_framework',
     'django_facebook',
     'storages',
